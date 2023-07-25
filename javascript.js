6766fac264ec797b887dc7fdb661cba3fd74cb62
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-07-25 13:01:41 -0700

    Write loop that creates div based on getTiles()
    The function createCanvas() takes the array created in getTiles()
    and creates a div for each of the items inside the div #canvas.
    Having both steps (calculating number of tiles needed and creating
    the appropriate number of div) being done through loops should enable
    me to adjust the canvas size easily on a later point.

--- javascript.js
+++ javascript.js
@@ -13,3 +13,18 @@ function getTiles(canvasHeight, canvasWidth) {
 }
 
 console.log(getTiles(16, 16));
+
+//Loop that takes tileArray and creates a div element for each of the array items
+
+function createCanvas(array) {
+  const canvas = document.querySelector("#canvas");
+  for (i = 0; i < array.length; i++) {
+    let div = document.createElement("div");
+    div.classList.add("tile");
+    div.setAttribute('id', `tile${[i]}`);
+    div.innerHTML = `tile${[i]}`;
+    canvas.appendChild(div);
+  }
+}
+
+createCanvas(getTiles(16, 16));
