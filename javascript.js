f3965db860a27cdf2e9a68219fd530b8afd75f2f
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-07-31 11:04:43 -0700

    Implement rainbow color option
    I implemented the option to set the paint color to "Rainbow".
    Activated the color that will be applied when the user paints is
    randomly chosen, every time a tiles is selected. So every tile will be
    painted in a different color.

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@
 //Loop that creates Array based on height and width of desired canvas
 let canvasHeight = 32;
 let canvasWidth = 32;
-
 function getTiles(canvasHeight, canvasWidth) {
   let tileArray = [];
   for (let i = 0; i <= (canvasHeight*canvasWidth-1); i++) {
@@ -26,8 +25,10 @@ function createCanvas(array) {
 }
 createCanvas(getTiles(canvasHeight, canvasWidth));
 
+
 //Create NodeList to ass all tiles
 const tiles = document.querySelectorAll(".tile");
+
 
 // Set tile size trough function to make grit size adjustable 
 function getTileSize() {
@@ -37,21 +38,60 @@ function getTileSize() {
 }
 getTileSize();
 
+
+// Rainbow button
+const btnRainbow = document.querySelector("#rainbow");
+let rainbowOn = false;
+  //changing btn colors depending if on or off
+function rainbowBtnColor() {
+  if (rainbowOn) {
+    btnRainbow.style.color = 'white';
+    btnRainbow.style.backgroundColor = 'black';
+  } else {
+    btnRainbow.style.color = 'black';
+    btnRainbow.style.backgroundColor = 'white';
+  }
+}
+function toggleRainbow() {
+  rainbowOn = !rainbowOn;
+  rainbowBtnColor();
+  console.log(rainbowOn)
+}
+btnRainbow.addEventListener('click', toggleRainbow);
+  //Create "Rainbow" color
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+function rainbowGenerator() {
+  const randomColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  return randomColor;
+}
+
+
 //Implement drawing function 
-  //First two eventListener register if mouse is pressed and let user draw continuously
 let mouseIsDown = false;
 for (let i = 0; i < tiles.length; i++) {
+  //First two eventListener register if mouse is pressed and let user draw continuously
   tiles[i].addEventListener('mousedown', () => {mouseIsDown = true});
   tiles[i].addEventListener('mouseup', () => {mouseIsDown = false});
+  // including the rainbow mode 
   tiles[i].addEventListener('click', (e) => {
-    e.target.style.backgroundColor = paintColor;
-  });
-  tiles[i].addEventListener('mouseover', (e) => {
-    if (mouseIsDown) {
+    if (rainbowOn) {
+    e.target.style.backgroundColor = rainbowGenerator();
+    } else {
       e.target.style.backgroundColor = paintColor;
     }
-  });
+});
+  tiles[i].addEventListener('mouseover', (e) => {
+    if (mouseIsDown) {
+      if (rainbowOn) {
+      e.target.style.backgroundColor = rainbowGenerator();
+      } else {
+        e.target.style.backgroundColor = paintColor;
+      }
+  }})
 }
+
 
 // Let user use buttons to set paint color
   //Still have to find a way to change the hover color, too
@@ -74,4 +114,3 @@ btnWhite.addEventListener('click', () => {
   paintColor = "white";
   return paintColor;
 });
-
