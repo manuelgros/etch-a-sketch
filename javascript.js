a478b1ab07c81a79af55b1594d58d2fc963eff06
manuelgros <131806742+manuelgros@users.noreply.github.com>
2023-07-31 16:35:38 -0700

    Introduce option to set grit size through buttons
    I implemented buttons that let user choose from a preset of grit sizes.
    If pressed the program deletes the initial tiles and fills canvas with
    the adjusted amount of new tiles, therefor making the canvas resolution
    bigger or smaller.
    For this to work correctly I had to make various changes on the
    existing code, mainly create function for some of the canvas creation
    steps and including the variable declaration for 'tiles', so that I can
    repeatably access them and the value is updated with each new grit
    creation.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,17 @@
 //Create the Grid for #canvas
   //Set Size for Canvas
-let canvasHeight = 32;
-let canvasWidth = 32; 
+let canvasHeight = 16;
+let canvasWidth = 16;
   //Loop that creates Array based on height and width of desired canvas
-function getTiles(canvasHeight, canvasWidth) {
+function getTileArray(height, width) {
   let tileArray = [];
-  for (let i = 0; i <= (canvasHeight*canvasWidth-1); i++) {
+  for (let i = 0; i <= (height*width-1); i++) {
     tileArray.push(i);
   }
   return tileArray;
 }
   //Loop that takes tileArray and creates a div (tiles of grit)
-function createCanvas(array) {
+function getTileElements(array) {
   const canvas = document.querySelector("#canvas");
   for (i = 0; i < array.length; i++) {
     let div = document.createElement("div");
@@ -20,16 +20,49 @@ function createCanvas(array) {
     // div.innerHTML = `•`;
     canvas.appendChild(div);
   }
+  // let tiles = document.querySelectorAll(".tile");
+  // return tiles;
 }
-createCanvas(getTiles(canvasHeight, canvasWidth));
+getTileElements(getTileArray(canvasHeight, canvasWidth)); //initial function call
+  
   // Set tile size trough function to make grit size adjustable 
-const tiles = document.querySelectorAll(".tile");
-function getTileSize() {
+let tiles = document.querySelectorAll(".tile");
+function getTileSize(height, width) {
+  let tiles = document.querySelectorAll(".tile");
   for (let i = 0; i < tiles.length; i++) {
-    tiles[i].setAttribute("style",`height:${480/canvasHeight}px; width:${480/canvasWidth}px`);
+    // tiles[i].setAttribute("style",`height:${480/height}px; width:${480/width}px`);
+    tiles[i].style.width = `${480/height}px`;
+    tiles[i].style.height = `${480/width}px`;
   }
 }
-getTileSize();
+getTileSize(canvasHeight, canvasWidth); //Initial function call
+
+  //bringing together the single steps of canvas creations
+function createCanvas (height, width) {
+  getTileElements(getTileArray(height, width));
+  getTileSize(height, width);
+}
+
+  //Deleting old Tiles (necessary when creating Canvas with different sized grit)
+function deleteTiles() {
+  let tiles = document.querySelectorAll(".tile");
+  for (let i = 0; i < tiles.length; i++) {
+    document.getElementById(`tile${[i]}`).remove();
+  }
+}
+
+//Set grit Size to button selection
+const btnSize = document.querySelectorAll(".sizes");
+for (i = 0; i < btnSize.length; i++) {
+  btnSize[i].addEventListener('click', (e) => {
+    deleteTiles();//deleting old grit
+    let canvasHeight = e.target.getAttribute('data-size');//set height
+    let canvasWidth = e.target.getAttribute('data-size');//set width
+    getTileElements(getTileArray(canvasHeight, canvasWidth));//getting tile elements with new arguments
+    getTileSize(canvasHeight, canvasWidth);//set tile height/width based on new dimensions
+    draw();
+  })
+} 
 
 
 // Rainbow button
@@ -63,32 +96,36 @@ function rainbowGenerator() {
 
 //Implement drawing function 
 let mouseIsDown = false;
-for (let i = 0; i < tiles.length; i++) {
-  //First two eventListener register if mouse is pressed and let user draw continuously
-  tiles[i].addEventListener('mousedown', () => {mouseIsDown = true});
-  tiles[i].addEventListener('mouseup', () => {mouseIsDown = false});
-  // including the rainbow mode 
-  tiles[i].addEventListener('click', (e) => {
-    if (rainbowOn) {
-    e.target.style.backgroundColor = rainbowGenerator();
-    } else {
-      e.target.style.backgroundColor = paintColor;
-    }
-});
-  tiles[i].addEventListener('mouseover', (e) => {
-    if (mouseIsDown) {
+function draw() {
+  let tiles = document.querySelectorAll(".tile");
+  for (let i = 0; i < tiles.length; i++) {
+    //First two eventListener register if mouse is pressed and let user draw continuously
+    tiles[i].addEventListener('mousedown', () => {mouseIsDown = true});
+    tiles[i].addEventListener('mouseup', () => {mouseIsDown = false});
+    // including the rainbow mode 
+    tiles[i].addEventListener('click', (e) => {
       if (rainbowOn) {
       e.target.style.backgroundColor = rainbowGenerator();
       } else {
         e.target.style.backgroundColor = paintColor;
       }
-  }})
+  });
+    tiles[i].addEventListener('mouseover', (e) => {
+      if (mouseIsDown) {
+        if (rainbowOn) {
+        e.target.style.backgroundColor = rainbowGenerator();
+        } else {
+          e.target.style.backgroundColor = paintColor;
+        }
+    }})
+  }
 }
+draw(); //initial function call
 
 
 // Let user use buttons to set paint color
   //Still have to find a way to change the hover color, too
-let paintColor = "black";
+let paintColor = "black"; //initially selected color
 const btnBlack = document.querySelector("#black");
 const btnRed = document.querySelector("#red");
 const btnWhite = document.querySelector("#white");
